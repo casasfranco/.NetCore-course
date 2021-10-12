c83e1637676b63db73ea38dd2e765843917bd918
Franco Casas <franco.casas@jbknowledge.com>
2021-10-12 08:40:15 -0300

    seccion 33

--- Aplicacion/Seguridad/UsuarioActual.cs
+++ Aplicacion/Seguridad/UsuarioActual.cs
@@ -1,22 +1,28 @@
-﻿using Aplicacion.Contratos;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using Aplicacion.Contratos;
 using Dominio;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Persistencia;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Aplicacion.Seguridad
 {
     public class UsuarioActual
     {
         public class Ejecutar : IRequest<UsuarioData> { }
+
         public class Manejador : IRequestHandler<Ejecutar, UsuarioData>
         {
             private readonly UserManager<Usuario> _userManager;
             private readonly IJwtGenerador _jwtGenerador;
             private readonly IUsuarioSesion _usuarioSesion;
+
             private readonly CursosOnlineContext _context;
             public Manejador(UserManager<Usuario> userManager, IJwtGenerador jwtGenerador, IUsuarioSesion usuarioSesion, CursosOnlineContext context)
             {
@@ -24,33 +30,15 @@ namespace Aplicacion.Seguridad
                 _jwtGenerador = jwtGenerador;
                 _usuarioSesion = usuarioSesion;
                 _context = context;
-
             }
             public async Task<UsuarioData> Handle(Ejecutar request, CancellationToken cancellationToken)
             {
                 var usuario = await _userManager.FindByNameAsync(_usuarioSesion.ObtenerUsuarioSesion());
-                Console.WriteLine(usuario);
-                return new UsuarioData
-                {
-                    NombreCompleto = "Franco",
-                    Username = "casassfranco",
-                    Token = "wqeqweqwdasdasdTOKENasdasdasd",
-                    Email = "Franco"
-                };
-                /*
-                var usuario = await _userManager.FindByNameAsync(_usuarioSesion.ObtenerUsuarioSesion());
+
                 var resultadoRoles = await _userManager.GetRolesAsync(usuario);
                 var listaRoles = new List<string>(resultadoRoles);
 
                 var imagenPerfil = await _context.Documento.Where(x => x.ObjetoReferencia == new Guid(usuario.Id)).FirstOrDefaultAsync();
-                return new UsuarioData
-                {
-                    NombreCompleto = usuario.NombreCompleto,
-                    Username = usuario.UserName,
-                    Token = _jwtGenerador.CrearToken(usuario, listaRoles),
-                    Email = usuario.Email
-                };
-                
                 if (imagenPerfil != null)
                 {
                     var imagenCliente = new ImagenGeneral
@@ -79,9 +67,7 @@ namespace Aplicacion.Seguridad
                         Email = usuario.Email
                     };
                 }
-                */
             }
         }
-
     }
 }
